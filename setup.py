de0faca5eb8b901ead473b077ad0388a007263e9
Matt Hayden (Valenceo LTD.) <https://github.com/matt-hayden>
2018-02-10 15:28:54 -0700

    Fixed old naming

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import find_packages, setup
 
-setup(name='UrlList',
+setup(name='UrlSort',
       version = "0.0.1",
-      description='Recognize familiar filename patterns in URI links',
-      url='https://github.com/matt-hayden/url_list',
+      description='Recognize familiar filename patterns in URLs',
+      url='https://github.com/matt-hayden/url_sort',
       maintainer="Matt Hayden (Valenceo, LTD.)",
       maintainer_email="github.com/matt-hayden",
       license='Unlicense',
       packages=find_packages(),
       entry_points = {
           'console_scripts': [
-              'urlsort=url_list.cli:main',
+              'urlsort=url_sort.cli:main',
               ]
       },
       zip_safe=True
